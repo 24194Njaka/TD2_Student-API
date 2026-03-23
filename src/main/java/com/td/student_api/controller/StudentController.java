9c0a63edcc56893b1e1c6c6a47c426f5928ef15a
Njaka <hei.herinjaka@gmail.com>
2026-03-23 21:40:28 +0300

    feat: add get students

--- src/main/java/com/td/student_api/controller/StudentController.java
+++ src/main/java/com/td/student_api/controller/StudentController.java
@@ -7,6 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -42,5 +43,36 @@ public class StudentController {
                     .body("erruer servuer : "  + e.getMessage());
 
         }
+    }
+
+    @GetMapping(value = "/students" , produces = {"text/plain" , "application/json"} )
+    public ResponseEntity<?> getStudents(@RequestHeader(value = "Accept" , required = false) String accept) {
+        try{
+            if(accept == null || accept.isEmpty()) {
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST)
+                        .body("header manquant");
+            }
+            if(accept.equals("text/plain")) {
+                String names = studentServer.getStudents()
+                        .stream()
+                        .map(student -> student.getFirstName() + " " + student.getLastName())
+                        .collect(Collectors.joining(", "));
+                return ResponseEntity.ok()
+                        .header("content-type", "text/plain")
+                        .body(names);
+            } else if (accept.equals("application/json")) {
+                return ResponseEntity.ok(studentServer.getStudents());
+                
+            } else {
+                return ResponseEntity.status(HttpStatus.NOT_IMPLEMENTED)
+                        .body("format no supporté");
+            }
+
+        } catch (Exception e){
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body("erruer servuer : "  + e.getMessage());
+
+        }
+
     }
 }
